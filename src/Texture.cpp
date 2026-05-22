ba5ab2870463992f1c3e8bc5851ec6925ffd2718
Kenny Le <154klee.business@gmail.com>
2026-05-22 09:58:43 -0500

    basic texture implementations; need to rewrite material.hpp and sceneloader to be compatible with textures

--- src/Texture.cpp
+++ src/Texture.cpp
@@ -1,1 +1,4 @@
 #include "Texture.hpp"
+
+using namespace std;
+
--- src/Texture.hpp
+++ src/Texture.hpp
@@ -3,16 +3,24 @@
 
 #include "Image.hpp"
 
+// Interface-ish
 class Texture {
 public:
-    Texture() {};
+    Texture() : color(0.0f), img(blankImage) {};
+    Texture(const std::string &str) : Texture() { init(str); }
+    Texture(const std::shared_ptr<Image> &img) : Texture() { init(img); } 
+    Texture(const glm::vec3& clr) : Texture() { init(clr); }
+
     virtual ~Texture() = default;
 
-    // Load an image file
-    void load(const std::string&);
+    // Load an image file and assigns it to this texture
+    void init(const std::string &str) { img = std::make_shared<Image>(str); };
+
+    // Assigns an image to this texture
+    void init(const std::shared_ptr<Image> &img) { this->img = img; };
 
     // Initialize with a flat color
-    void load(const glm::vec3&);
+    void init(const glm::vec3 &clr) {this->color = clr; };
 
     virtual glm::vec3 value(float u, float v, const glm::vec3& p) const = 0;
 
@@ -29,9 +37,7 @@ public:
     ColorTexture() {};
     virtual ~ColorTexture() = default;
 
-    glm::vec3 value(float, float, const glm::vec3&) const override { 
-        return color; 
-    }
+    glm::vec3 value(float, float, const glm::vec3&) const override { return color; }
 };
 
 class SpatialTexture : public Texture {
@@ -39,7 +45,8 @@ public:
     SpatialTexture() {};
     virtual ~SpatialTexture() = default;
 
-    glm::vec3 value(float, float, const glm::vec3& p) const override {
+    glm::vec3 value(float u, float v, const glm::vec3& p) const override {
+        // TODO: Checkers?
         return glm::vec3(0.0f);
     }
 };
@@ -50,6 +57,12 @@ public:
     virtual ~ImageTexture() = default;
 
     glm::vec3 value(float u, float v, const glm::vec3&) const override {
-        return glm::vec3(0.0f);
+        #ifndef NDEBUG
+        if (!img->isLoaded()) return glm::vec3(1.0f, 0.0f, 1.0f);
+        #endif 
+        // Just realized that img->getPixel does most of the work haha
+        glm::vec3 pixelColor;
+        img->getPixel(u, v, pixelColor);
+        return pixelColor;
     }
 };
