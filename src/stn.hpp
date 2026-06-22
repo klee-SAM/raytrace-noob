f54104d8ee022a1d5de9037ccd886323b5aa1676
Kenny Le <154klee.business@gmail.com>
2026-06-21 21:48:33 -0500

    moved vec and mat typedefs into common

--- src/stn.hpp
+++ src/stn.hpp
@@ -26,3 +26,9 @@
 #include <glm/gtc/random.hpp>
 #include <glm/gtc/constants.hpp>
 #include <glm/gtx/string_cast.hpp>
+
+typedef glm::vec2 vec2;
+typedef glm::vec3 vec3;
+typedef glm::vec4 vec4;
+typedef glm::mat3 mat3;
+typedef glm::mat4 mat4;
