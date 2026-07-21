5381cf54deafca3a1cf6ca78cd543dc9ad7df4ec
Kenny Le <154klee.business@gmail.com>
2026-07-20 23:05:49 -0500

    more comtemplation

--- src/tracers/Raytracer.hpp
+++ src/tracers/Raytracer.hpp
@@ -3,24 +3,76 @@
 #define RAYTRACER_H
 
 #include "../stn.hpp"
-#include "Texture.hpp"
+#include "../util/umath.hpp"
+#include "../util/RowQueue.hpp"
+#include "../Ray.hpp"
+#include "../Texture.hpp"
+
+class Scene;
+class Light;
 
 // Common functionality for all raytracers.
 
 class Raytracer {
 public:
+    typedef const glm::mat4 CMatrix;
 
-enum class SkyType {Void, Haze, SphereMap, Ambient};
+    // having a lower epsilion value does not bode well with meshes
+    // TODO: test if this is still the case
+    const float EPSILION = 5E-4f; // 5E-3f was prev val
+    const float MAX_DIST = CONSTANTS::MAX;
+    const uint MAX_RECURSIONS = 7;
+    const float MINIMUM_COEFF = 0.005f;
 
-// todo:
+    Raytracer() {};
+    virtual ~Raytracer() = default;
 
-private:
-    uint AAsamples = 1;                        // must be at least 1
-    uint occlusionSamples = 0;                 // actual count is divided by AA samples
-    float occludingRadius = 0.25f;             // radius of occluding hemisphere
-    glm::vec3 globalAmbient = glm::vec3(0.0f); // ambient color added to all objects.
-    SkyType sky = SkyType::Ambient;
-    std::unique_ptr<ImageTexture> skyTexture;
+    // todo:
+    // perhaps this takes a struct as an argument which contains the 
+    // P and MV matrices, scene, and camera...
+    // or perhaps the camera holds the P and MV matrices
+    std::unique_ptr<Image> render(std::unique_ptr<Scene>&, CMatrix &P, CMatrix &MV) const;
+    void processRows(const std::unique_ptr<Scene> &scene, std::unique_ptr<Image> &image);
+    virtual void setRow(const std::unique_ptr<Scene> &scene, std::unique_ptr<Image> &image, uint y) = 0;
+
+    // TODO: for cast secondary ray, need to copy some members of camera
+    // as the raytracer's members so that castSecondary ray can be used;
+    // want to minimize the overhead for this 
+
+    // Might move all these methods to private, and just have getRayColor be the function that
+    // actually needs to be overridden. 
+    Ray castPrimaryRay(uint idx, uint idy, const glm::vec2 &offset = glm::vec2(.5f)) const;
+    Ray castSecondaryRay(const Ray &primaryRay) const;
+
+    // what was deleted here should be in camera, actually
+    // (following raytracing in one weekend example)
+
+    // the sky should belong in Scene.hpp,
+    // camera settings should become public
+    // make variables computed in render() as
+    // protected variables here, because I
+    // am not going to bother defining a whole
+    // set of getters and setters for them
+
+protected:
+    RowQueue r_queue;
+
+    // variables computed in render()
+    glm::vec4 cameraPos; // contains world-space position of camera
+    glm::mat4 C;         // Camera Matrix, inverse of View Matrix
+    glm::mat4 invP;      // inverse of projection mat
+    glm::vec4 dof_u;     // right cam basis vec
+    glm::vec4 dof_v;     // up cam basis vec
+    float sample_scale;
+
+    // Contains common info used for BRDF calculations
+    struct IntParams {
+        const std::unique_ptr<Scene> &scene;
+        const Interval &interval;
+        const Hit &rec;
+    };  
+
+    // ...
 };
 
 #endif
